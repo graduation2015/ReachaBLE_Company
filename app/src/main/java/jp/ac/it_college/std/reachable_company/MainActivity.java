395c9bcee6af89aa1b1c5b03898e7f381c1ba38c
s13012 <s13012@std.it-college.ac.jp>
2015-10-26 16:12:01 +0900

    メイン画面にDrawerLayout実装

--- app/src/main/java/jp/ac/it_college/std/reachable_company/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/reachable_company/MainActivity.java
@@ -1,11 +1,11 @@
 package jp.ac.it_college.std.reachable_company;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
