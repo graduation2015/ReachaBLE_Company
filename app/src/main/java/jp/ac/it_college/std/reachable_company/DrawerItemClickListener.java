ac31abd7e663e6ae02dfd750e2239a9876d5636d
s13012 <s13012@std.it-college.ac.jp>
2015-10-27 16:15:40 +0900

    add DrawerItemClickListener.java

---
+++ app/src/main/java/jp/ac/it_college/std/reachable_company/DrawerItemClickListener.java
@@ -0,0 +1,15 @@
+package jp.ac.it_college.std.reachable_company;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class DrawerItemClickListener implements AdapterView.OnItemClickListener {
+
+    private ListView mDrawerList;
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+}
--- app/src/main/java/jp/ac/it_college/std/reachable_company/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/reachable_company/MainActivity.java
@@ -1,37 +1,90 @@
 package jp.ac.it_college.std.reachable_company;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //DrawerLayout関連
+    private String[] mPlanetTitles;
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    //Toolbar関連
+    private Toolbar mToolbar;
+    private CharSequence mTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    /**
+     * DrawerLayoutをセットアップする
+     */
+    private void setUpDrawerLayout() {
+        //ページのタイトルを取得
+        mTitle = getTitle();
+
+        //ActionBarDrawerToggleを生成
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar
+                ,R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //ドロワーを開いた時に呼ばれる
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                //ドロワーを閉じた時に呼ばれる
+
+            }
+        };
+        //DrawerListenerにDrawerToggleをセット
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        //サイドメニューに表示するメニューをリソースから取得
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+
+        //サイドメニューリスト用アダプターをセット
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, mPlanetTitles);
+        mDrawerList.setAdapter(listAdapter);
+
+        //サイドメニューのonClickListenerをセット
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    /**
+     * Toolbarをアクションバーとしてセットする
+     */
+    private void setUpToolbar() {
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    /**
+     * レイアウトからViewを取得する
+     */
+    private void findViews() {
+        mDrawerList = (ListView) findViewById(R.id.side_menu_list);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
     }
 }
