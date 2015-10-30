ae61779fe82586c90d1aca5ae052be0470df8f91
s13012 <s13012@std.it-college.ac.jp>
2015-10-30 16:45:35 +0900

    クーポン選択ボタンの処理を実装

--- app/src/main/java/jp/ac/it_college/std/reachable_company/CouponUploadFragment.java
+++ app/src/main/java/jp/ac/it_college/std/reachable_company/CouponUploadFragment.java
@@ -1,21 +1,91 @@
 package jp.ac.it_college.std.reachable_company;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
-public class CouponUploadFragment extends Fragment {
+public class CouponUploadFragment extends Fragment implements View.OnClickListener {
+
+    /** ギャラリー用の定数 */
+    private static final int REQUEST_GALLERY = 0;
+
+    /** Views */
+    private Button couponSelectButton;
+    private Button couponUploadButton;
+    private ImageView couponPreview;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coupon_upload, container, false);
+        View contentView = inflater.inflate(R.layout.fragment_coupon_upload, container, false);
+        findViews(contentView);
+
+        return contentView;
     }
 
+    /**
+     * レイアウトからViewを取得する
+     */
+    private void findViews(View contentView) {
+        couponSelectButton = (Button) contentView.findViewById(R.id.btn_coupon_select);
+        couponUploadButton = (Button) contentView.findViewById(R.id.btn_coupon_upload);
+        couponPreview = (ImageView) contentView.findViewById(R.id.img_coupon_preview);
 
+        couponSelectButton.setOnClickListener(this);
+        couponUploadButton.setOnClickListener(this);
+    }
+
+    /**
+     * クーポンを選択する
+     */
+    private void couponSelect() {
+        Intent intent = new Intent();
+
+        //APILevelが19(version 4.4)以上の場合
+        if (Build.VERSION.SDK_INT >= 19) {
+            intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+            intent.setType("*/*");
+            String[] mimeTypes = {"image/jpeg", "image/png"};
+            intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+        } else {
+            //APILevelが18(version 4.3)以下の場合
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+            intent.setType("image/*");
+        }
+
+        startActivityForResult(intent, REQUEST_GALLERY);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_GALLERY && resultCode == Activity.RESULT_OK) {
+
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_coupon_select:
+                couponSelect();
+                break;
+            case R.id.btn_coupon_upload:
+                break;
+        }
+    }
 }
