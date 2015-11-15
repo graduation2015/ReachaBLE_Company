5fe0abe022d5e5c2383ca5548656f83570ff12d8
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-11-15 20:39:30 +0900

    Add S3UploadManager

---
+++ app/src/main/java/jp/ac/it_college/std/reachable_company/S3UploadManager.java
@@ -0,0 +1,25 @@
+package jp.ac.it_college.std.reachable_company;
+
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+
+import java.io.File;
+
+public class S3UploadManager {
+    public static final String FOLDER_SUFFIX = "/";
+    private final TransferUtility mTransferUtility;
+
+    public S3UploadManager(TransferUtility transferUtility) {
+        this.mTransferUtility = transferUtility;
+    }
+
+    public TransferObserver upload(File file) {
+        TransferObserver observer = mTransferUtility.upload(
+                Constants.BUCKET_NAME,
+                file.getName(),
+                file);
+
+        return observer;
+    }
+
+}
