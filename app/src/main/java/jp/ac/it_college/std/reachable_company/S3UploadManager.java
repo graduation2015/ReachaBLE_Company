2725143fa1c8c72c533755b0aa7477219fe37b4d
s13012 <s13012@std.it-college.ac.jp>
2015-11-16 11:38:25 +0900

    "企業ID/企業ID.拡張子" の形式でファイルをアップロードするように修正

--- app/src/main/java/jp/ac/it_college/std/reachable_company/S3UploadManager.java
+++ app/src/main/java/jp/ac/it_college/std/reachable_company/S3UploadManager.java
@@ -7,19 +7,38 @@ import java.io.File;
 
 public class S3UploadManager {
     public static final String FOLDER_SUFFIX = "/";
+    public static final String FILE_DELIMITER = ".";
     private final TransferUtility mTransferUtility;
 
     public S3UploadManager(TransferUtility transferUtility) {
         this.mTransferUtility = transferUtility;
     }
 
+    /**
+     * 渡されたファイルをS3バケットにアップロードする
+     * @param file
+     * @return
+     */
     public TransferObserver upload(File file) {
+        // "企業ID/企業ID.拡張子" の形式でファイルをアップロードする
+
         TransferObserver observer = mTransferUtility.upload(
                 Constants.BUCKET_NAME,
-                CompanyInfo.COMPANY_ID + FOLDER_SUFFIX + file.getName(),
+                CompanyInfo.COMPANY_ID + FOLDER_SUFFIX +
+                        CompanyInfo.COMPANY_ID + getExtension(file),
                 file);
 
         return observer;
     }
 
+    /**
+     * 渡されたファイルの拡張子を文字列で返す
+     * @param file
+     * @return
+     */
+    private String getExtension(File file) {
+        int index = file.getName().lastIndexOf(FILE_DELIMITER);
+
+        return file.getName().substring(index);
+    }
 }
