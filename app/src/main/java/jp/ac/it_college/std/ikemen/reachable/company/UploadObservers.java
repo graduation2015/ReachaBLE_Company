31341683186a039880df87f8b589c75fb5271b3b
s13012 <s13012@std.it-college.ac.jp>
2015-12-02 10:35:54 +0900

    UploadObserverクラスのフィールドにfinal修飾子追加し、フィールドの初期化をコンストラクタでするように修正

--- app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/UploadObservers.java
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/UploadObservers.java
@@ -2,11 +2,10 @@ package jp.ac.it_college.std.ikemen.reachable.company;
 
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class UploadObservers {
-    private List<TransferObserver> observers = new ArrayList<>();
+    private final List<TransferObserver> observers;
 
     public UploadObservers(List<TransferObserver> observers) {
         this.observers = observers;
