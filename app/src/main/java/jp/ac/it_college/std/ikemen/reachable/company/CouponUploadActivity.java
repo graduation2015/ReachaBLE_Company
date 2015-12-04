de0218ee121d44a71732affce00572b5de152069
s13012 <s13012@std.it-college.ac.jp>
2015-12-04 13:42:35 +0900

    Add CouponUploadActivity

---
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/CouponUploadActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.it_college.std.ikemen.reachable.company;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CouponUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coupon_upload);
+    }
+
+}
