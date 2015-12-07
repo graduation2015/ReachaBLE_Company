6bc18efbff3ca04735ae642b33818af6ec1bacfb
s13012 <s13012@std.it-college.ac.jp>
2015-12-07 14:26:35 +0900

    ギャラリーで選択された画像をクーポン作成画面で表示する処理を実装

--- app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/CouponUploadActivity.java
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/CouponUploadActivity.java
@@ -1,16 +1,21 @@
 package jp.ac.it_college.std.ikemen.reachable.company;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Toast;
+import android.widget.ImageView;
+
+import java.net.URISyntaxException;
+
+import jp.ac.it_college.std.ikemen.reachable.company.util.FileUtil;
 
 public class CouponUploadActivity extends AppCompatActivity {
 
     /* Views */
     private Toolbar mToolbar;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +27,23 @@ public class CouponUploadActivity extends AppCompatActivity {
 
     private void initSettings() {
         setUpToolbar();
+        setCouponPreview();
     }
+
+    /**
+     * 選択されたクーポン画像をプレビューにセット
+     */
+    private void setCouponPreview() {
+        String path = null;
+        try {
+            path = FileUtil.getPath(this, getIntent().getData());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        getImageView().setImageBitmap(BitmapFactory.decodeFile(path));
+    }
+
 
     private void setUpToolbar() {
         setSupportActionBar(getToolbar());
@@ -34,6 +55,13 @@ public class CouponUploadActivity extends AppCompatActivity {
             mToolbar = (Toolbar) findViewById(R.id.toolbar);
         }
         return mToolbar;
+    }
+
+    public ImageView getImageView() {
+        if (mImageView == null) {
+            mImageView = (ImageView) findViewById(R.id.img_coupon_preview);
+        }
+        return mImageView;
     }
 
     @Override
