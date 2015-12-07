8d72e29b02f9b5ef95a7483ea7255dd2e7f1f788
s13012 <s13012@std.it-college.ac.jp>
2015-12-07 12:03:18 +0900

    CouponUploadActivityに戻るボタンを設置

--- app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/CouponUploadActivity.java
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/CouponUploadActivity.java
@@ -2,13 +2,37 @@ package jp.ac.it_college.std.ikemen.reachable.company;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class CouponUploadActivity extends AppCompatActivity {
+
+    /* Views */
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coupon_upload);
+
+        initSettings();
+    }
+
+    private void initSettings() {
+        setUpToolbar();
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar(getToolbar());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public Toolbar getToolbar() {
+        if (mToolbar == null) {
+            mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        }
+        return mToolbar;
     }
 
 }
