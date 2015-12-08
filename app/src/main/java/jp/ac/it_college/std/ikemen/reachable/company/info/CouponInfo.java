49104d6c55d0274dc7acaec79aea7fad5bd21ec6
s13012 <s13012@std.it-college.ac.jp>
2015-12-08 11:45:31 +0900

    CouponInfoクラスにフィールド(title, description)を追加

--- app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/info/CouponInfo.java
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/info/CouponInfo.java
@@ -6,21 +6,34 @@ public class CouponInfo {
     private final String key;
     private final String companyName;
     private final String address;
+    private final String title;
+    private final String description;
     private final List<String> category;
 
     public static final String COMPANY_NAME = "companyName";
     public static final String ADDRESS = "address";
     public static final String CATEGORY = "category";
+    public static final String TITLE = "category";
+    public static final String DESCRIPTION = "category";
 
 
     public CouponInfo(List<String> category) {
-        this(CompanyInfo.COMPANY_ID, CompanyInfo.COMPANY_NAME, CompanyInfo.COMPANY_ADDRESS, category);
+        this(CompanyInfo.COMPANY_ID, CompanyInfo.COMPANY_NAME,
+                CompanyInfo.COMPANY_ADDRESS, null, null, category);
     }
 
-    public CouponInfo(String key, String companyName, String address, List<String> category) {
+    public CouponInfo(String title, String description, List<String> category) {
+        this(CompanyInfo.COMPANY_ID, CompanyInfo.COMPANY_NAME,
+                CompanyInfo.COMPANY_ADDRESS, title, description, category);
+    }
+
+    public CouponInfo(String key, String companyName, String address,
+                      String title, String description, List<String> category) {
         this.key = key;
         this.companyName = companyName;
         this.address = address;
+        this.title = title;
+        this.description = description;
         this.category = category;
     }
 
@@ -38,5 +51,13 @@ public class CouponInfo {
 
     public List<String> getCategory() {
         return category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
