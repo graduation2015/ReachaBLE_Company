7a1946dc9a60649685e2092d014b38ce2c51bb68
s13012 <s13012@std.it-college.ac.jp>
2015-12-08 13:25:52 +0900

    クーポン作成画面のクーポン情報を入力するEditTextのゲッターを実装 Json書き込み用のテストボタンを配置

--- app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/CouponUploadActivity.java
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/CouponUploadActivity.java
@@ -5,17 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
 
+import jp.ac.it_college.std.ikemen.reachable.company.info.CouponInfo;
+import jp.ac.it_college.std.ikemen.reachable.company.json.JsonManager;
 import jp.ac.it_college.std.ikemen.reachable.company.util.FileUtil;
 
-public class CouponUploadActivity extends AppCompatActivity {
+public class CouponUploadActivity extends AppCompatActivity implements View.OnClickListener {
 
     /* Views */
     private Toolbar mToolbar;
     private ImageView mImageView;
+    private TextView mTitleView;
+    private TextView mDescriptionView;
+    private TextView mTagsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,8 @@ public class CouponUploadActivity extends AppCompatActivity {
     private void initSettings() {
         setUpToolbar();
         setCouponPreview();
+
+        findViewById(R.id.btn_test_put_json).setOnClickListener(this);
     }
 
     /**
@@ -69,5 +80,46 @@ public class CouponUploadActivity extends AppCompatActivity {
         //SENDメニューを表示
         getMenuInflater().inflate(R.menu.menu_coupon_upload, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_test_put_json:
+                putInfo();
+                break;
+        }
+    }
+
+    public TextView getTitleView() {
+        if (mTitleView == null) {
+            mTitleView = (TextView) findViewById(R.id.editText_coupon_title);
+        }
+        return mTitleView;
+    }
+
+    public TextView getDescriptionView() {
+        if (mDescriptionView == null) {
+            mDescriptionView = (TextView) findViewById(R.id.editText_coupon_description);
+        }
+        return mDescriptionView;
+    }
+
+    public TextView getTagsView() {
+        if (mTagsView == null) {
+            mTagsView = (TextView) findViewById(R.id.editText_coupon_tag);
+        }
+        return mTagsView;
+    }
+
+    private void putInfo() {
+        String title = getTitleView().getText().toString();
+        String description = getDescriptionView().getText().toString();
+        String tags = getTagsView().getText().toString();
+        List<String> tagsList = Arrays.asList(tags.replaceAll("　", " ").split(" "));
+
+        CouponInfo couponInfo = new CouponInfo(title, description, tagsList);
+        JsonManager manager = new JsonManager(this);
+        manager.putJsonObj(couponInfo);
     }
 }
