18243fc2adeb32693eff26618c78567c049c38eb
s13012 <s13012@std.it-college.ac.jp>
2015-12-14 15:19:30 +0900

    Update EmptySupportRecyclerView

--- app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/EmptySupportRecyclerView.java
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/EmptySupportRecyclerView.java
@@ -6,63 +6,65 @@ import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
 
+
 /**
  * リストが空の際に表示するメッセージに対応したRecyclerViewクラス
  */
 public class EmptySupportRecyclerView extends RecyclerView {
-    private View emptyView;
-
-    private AdapterDataObserver emptyObserver = new AdapterDataObserver() {
-        @Override
-        public void onChanged() {
-            Adapter<?> adapter =  getAdapter();
-            if(adapter != null && getEmptyView() != null) {
-                if(adapter.getItemCount() == 0) {
-                    getEmptyView().setVisibility(View.VISIBLE);
-                    EmptySupportRecyclerView.this.setVisibility(View.GONE);
-                }
-                else {
-                    getEmptyView().setVisibility(View.GONE);
-                    EmptySupportRecyclerView.this.setVisibility(View.VISIBLE);
-                }
-            }
-
-        }
-    };
+    @Nullable
+    private View mEmptyView;
 
     public EmptySupportRecyclerView(Context context) {
         super(context);
     }
 
-    public EmptySupportRecyclerView(Context context, @Nullable AttributeSet attrs) {
+    public EmptySupportRecyclerView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public EmptySupportRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+    public EmptySupportRecyclerView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
 
+    void checkIfEmpty() {
+        if (mEmptyView != null && getAdapter() != null) {
+            mEmptyView.setVisibility(getAdapter().getItemCount() > 0 ? GONE : VISIBLE);
+        }
+    }
 
-    @Override
-    public void setAdapter(Adapter adapter) {
-        super.setAdapter(adapter);
-
-        if (adapter != null) {
-            adapter.registerAdapterDataObserver(getEmptyObserver());
+    private AdapterDataObserver emptyObserver = new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            checkIfEmpty();
         }
 
-        getEmptyObserver().onChanged();
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            super.onItemRangeInserted(positionStart, itemCount);
+            checkIfEmpty();
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            super.onItemRangeRemoved(positionStart, itemCount);
+            checkIfEmpty();
+        }
+    };
+
+    @Override
+    public void setAdapter(@Nullable Adapter adapter) {
+        super.setAdapter(adapter);
+
+        if(adapter != null) {
+            adapter.registerAdapterDataObserver(emptyObserver);
+        }
+
+        emptyObserver.onChanged();
     }
 
-    public AdapterDataObserver getEmptyObserver() {
-        return emptyObserver;
-    }
-
-    public View getEmptyView() {
-        return emptyView;
-    }
-
-    public void setEmptyView(View emptyView) {
-        this.emptyView = emptyView;
+    public void setEmptyView(@Nullable View emptyView) {
+        this.mEmptyView = emptyView;
+        checkIfEmpty();
     }
 }
