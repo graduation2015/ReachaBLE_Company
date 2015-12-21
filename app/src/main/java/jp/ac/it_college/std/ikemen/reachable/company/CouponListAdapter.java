e2f6fde35aa35027a1032b62d79cf6b416ce0916
s13012 <s13012@std.it-college.ac.jp>
2015-12-21 15:22:51 +0900

    decodeSampledBitmapFromFileメソッドをFileUtilクラスへ移動した

--- app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/CouponListAdapter.java
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/CouponListAdapter.java
@@ -1,7 +1,5 @@
 package jp.ac.it_college.std.ikemen.reachable.company;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatTextView;
@@ -13,6 +11,7 @@ import android.view.ViewGroup;
 import java.util.List;
 
 import jp.ac.it_college.std.ikemen.reachable.company.info.CouponInfo;
+import jp.ac.it_college.std.ikemen.reachable.company.util.FileUtil;
 
 public class CouponListAdapter extends RecyclerView.Adapter<CouponListAdapter.CouponViewHolder>{
 
@@ -36,7 +35,7 @@ public class CouponListAdapter extends RecyclerView.Adapter<CouponListAdapter.Co
     @Override
     public void onBindViewHolder(CouponViewHolder holder, int position) {
         CouponInfo info = getCouponInfoList().get(position);
-        holder.mCouponPic.setImageBitmap(decodeSampledBitmapFromFile(
+        holder.mCouponPic.setImageBitmap(FileUtil.decodeSampledBitmapFromFile(
                 info.getFilePath(), BITMAP_WIDTH_SIZE, BITMAP_HEIGHT_SIZE));
         holder.mTitleView.setText(info.getTitle());
         holder.mDescriptionView.setText(info.getDescription());
@@ -48,54 +47,6 @@ public class CouponListAdapter extends RecyclerView.Adapter<CouponListAdapter.Co
     public int getItemCount() {
         return getCouponInfoList().size();
     }
-
-    /**
-     * 画像のサブサンプルサイズを計算して返す
-     * @param options デコードした後のoptions
-     * @param reqWidth 画像をセットするImageViewの横幅
-     * @param reqHeight 画像をセットするImageViewの縦幅
-     * @return 画像のサブサンプルサイズ
-     */
-    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // 画像の元サイズ
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-        return inSampleSize;
-    }
-
-
-    /**
-     * 画像をリサイズしてBitmapで返す
-     * @param filePath 読み込む画像のファイルパス
-     * @param reqWidth 画像をセットするImageViewの横幅
-     * @param reqHeight 画像をセットするImageViewの縦幅
-     * @return サンプルサイズを計算しなおしたBitmap
-     */
-    private Bitmap decodeSampledBitmapFromFile(String filePath, int reqWidth, int reqHeight) {
-        // inJustDecodeBounds=true で画像のサイズをチェック
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(filePath, options);
-
-        // inSampleSize を計算
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // inSampleSize をセットしてデコード
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(filePath, options);
-    }
-
 
     public List<CouponInfo> getCouponInfoList() {
         return mCouponInfoList;
