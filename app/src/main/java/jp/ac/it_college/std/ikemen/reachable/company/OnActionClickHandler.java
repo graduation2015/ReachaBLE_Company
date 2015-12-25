2ad5772fe19c75a02d64bb8bc135de00a93c9905
Akinori Maesoko <s13012@std.it-college.ac.jp>
2015-12-25 17:07:41 +0900

    Add OnActionClickHandler.java

---
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/OnActionClickHandler.java
@@ -0,0 +1,33 @@
+package jp.ac.it_college.std.ikemen.reachable.company;
+
+import android.view.View;
+
+/**
+ * クーポンアクションボタン押下時の処理を決定するハンドラークラス
+ */
+public class OnActionClickHandler implements View.OnClickListener {
+    private final CouponListAdapter.CouponViewHolder mHolder;
+    private final OnActionClickListener mListener;
+
+    public OnActionClickHandler(CouponListAdapter.CouponViewHolder mHolder, OnActionClickListener mListener) {
+        this.mHolder = mHolder;
+        this.mListener = mListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mListener == null) {
+            return;
+        }
+
+        switch (v.getId()) {
+            case R.id.btn_advertise:
+                mListener.onAdvertiseClick(mHolder.mAdvertiseButton, mHolder.getAdapterPosition());
+                break;
+            case R.id.btn_delete:
+                mListener.onDeleteClick(mHolder.mDeleteButton, mHolder.getAdapterPosition());
+                break;
+        }
+    }
+
+}
