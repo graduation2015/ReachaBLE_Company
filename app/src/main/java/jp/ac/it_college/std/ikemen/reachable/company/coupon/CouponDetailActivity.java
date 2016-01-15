a8fbaad5cb12c81569a80d1e2a3dd577785337ef
s13012 <s13012@std.it-college.ac.jp>
2016-01-15 15:33:32 +0900

    Add CouponDetailActivity.java

---
+++ app/src/main/java/jp/ac/it_college/std/ikemen/reachable/company/coupon/CouponDetailActivity.java
@@ -0,0 +1,16 @@
+package jp.ac.it_college.std.ikemen.reachable.company.coupon;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import jp.ac.it_college.std.ikemen.reachable.company.R;
+
+public class CouponDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coupon_detail);
+    }
+
+}
